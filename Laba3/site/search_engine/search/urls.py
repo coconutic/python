41e6cf8376c516b2ddd430b20ccbbb313d99dc69
KatteKurach <kattekurach@gmail.com>
2016-06-09 09:40:26 +0300

    Added sheet and search.

--- Laba3/site/search_engine/search/urls.py
+++ Laba3/site/search_engine/search/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.search, name='search'),        
+    url(r'^$', views.search, name='search'),
+    url(r'^search_results/$', views.find_res, name='find_res'),
+    url(r'^index_url/$', views.index_url, name='index_url'),
+    url(r'^urls/$', views.urls, name='urls'),
+    url(r'^change_index/$', views.change_index, name='change_index'),
 ]
--- Laba3/site/search_engine/search/views.py
+++ Laba3/site/search_engine/search/views.py
@@ -1,7 +1,31 @@
+import Search_str
+import json
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 
 
+def index_url(request):
+#    if (request.GET.url('add_url')):
+ #       t = "r"
+    return render(request, 'search/index_url.html')
+
+def find_res(request):
+    s = Search_str.Search(request.GET.get('text'))
+    ans=  s.start()
+    return HttpResponse(json.dumps({'urls': ans}), content_type="application/json")
+
+def urls(request):
+    return render(request, 'search/urls.html')
+
+def change_index(request):
+    return render(request, 'search/change_index.html')
 
 def search(request):
-    return render(request, 'search/search.html')
+    t = []
+    if (request.GET.get('search')):
+        temp = request.GET.get('str')
+        for i in xrange(3):
+            t.append(temp)
+    context = {'user': t}
+    return render(request, 'search/search.html', context)
+
