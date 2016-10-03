47b7836e333d19fb3f4a858f059df7ea18bdf148
KatteKurach <kattekurach@gmail.com>
2016-10-03 10:20:35 +0300

    Models added, page style ended.

--- skylines/timetable/models.py
+++ skylines/timetable/models.py
@@ -2,4 +2,26 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
+class User(models.Model):
+    id = models.IntegerField()
+    login = models.CharField(max_length = 10)
+    password = models.CharField(max_length = 20)
+    access_level = models.IntegerField()
+
+
+
+class Airplane(models.Model):
+    id = models.IntegerField()
+    cost = models.IntegerField()
+    seats = models.IntegerField()
+
+
+class Airport(models.Model):
+    id = models.IntegerFiled()
+    name = models.CharField(max_length = 10)
+    country = models.CharField(max_length = 10)
+
+class TimeTable(models.Model):
+    id_plane =  
 # Create your models here.
---
+++ skylines/timetable/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^main/$', views.main, name='main'),
+    url(r'^$', views.shedule, name='shedule'),
+]
